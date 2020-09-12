b57d5b2280a6304a70359b42e0990c2e59465c3d
Eric Ogden <eogden@maana.io>
2020-09-11 19:43:34 -0700

    websockets working with multi-part messages

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask
 from flask_socketio import SocketIO
 
@@ -15,5 +16,28 @@ def handle_message(message):
 @socketio.on('json')
 def handle_json(json):
     print('received json: ' + str(json))
+
+@socketio.on('save:json')
+def save_json(json):
+    print(f'got json: {str(json)}')
+    timers = [1, 2, 2.5, 3.5, 1.5, 3]
+    messages = [
+        'saving data...',
+        'still saving data...',
+        'taking a nep...',
+        'dreaming...',
+        'wrapping up...',
+        'reticulating splines...'
+    ]
+    total = len(timers)
+    for index in range(total):
+        socketio.emit('progress', f'{index + 1}/{total} - {messages[index]}')
+        time.sleep(timers[index])
+    socketio.emit('progress', 'done')
+
+@socketio.on('echo')
+def echo(message):
+    # socketio.send(message)
+    socketio.emit('echo', message)
 
 from app import views
